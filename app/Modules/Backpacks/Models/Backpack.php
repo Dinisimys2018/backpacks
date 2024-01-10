2365cc86347148612b7d950963f5a3fbd75f4b0c
Ratushniak DA <ratushniak_da@groupbwt.com>
2024-01-10 14:08:36 +0200

    init project

---
+++ app/Modules/Backpacks/Models/Backpack.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Backpacks\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+final class Backpack extends Model
+{
+    use HasFactory;
+}
